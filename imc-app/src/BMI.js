faff253b70253e6c6586ed6323f306a630890e6e
Jose Guerrero <wichoguerrero@yahoo.com>
2018-10-04 00:18:35 -0500

    second commit

--- imc-app/src/BMI.js
+++ imc-app/src/BMI.js
@@ -1,58 +1,41 @@
 import React from 'react';
 import {Component,Fragment} from 'react';
-import Altura from '../src/Components/Altura'
-import Peso from '../src/Components/Peso'
-import Nombre from '../src/Components/Nombre'
-import Boton from '/home/jose/Desktop/BEDU/imcreact/imc-app/src/Components/Boton.js';
-
 
 
 
 
 class BMI extends Component {
-  calcBMI = (event)=>{
-  BMI = this.state.valor / (this.state.valor1 * this.state.valor1);
-  return  console.log( BMI);
+  state = {
+    value1:'',
+    value:''
+  }
+
+  handleChange1 = (event) => {
+    this.setState({value1: event.target.value});
+    console.log(this.state.value1)
+  };
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+    console.log(this.state.value)
+  };
+
+  calcBMI = () => { 
+    BMI = this.state.value/(this.state.value1 * this.state.value1);
+    console.log(BMI);
+    return document.querySelector('#result').innerHTML = BMI;
   }
 
   render() {
     return(
       <Fragment>
-        <Altura />
-        <Peso />
-        <Boton onClick={this.calcBMI}/>
+        <input placeholder='Altura' value1={this.state.value1} onChange={this.handleChange1}/>
+        <input placeholder='Peso' value={this.state.value} onChange={this.handleChange}/>
+        <button onClick={this.calcBMI}>presione aqui</button>
+        <p id='result'></p>
       </Fragment>
     )
   }
-
-
-
-
-
-  //   state = {
-  //     valor:''
-  //   }
-  //       cambiarState = ({target})=>{
-  //         this.setState(()=>{
-  //           valor: target.value
-  //         })
-  //         console.log('target.value')
-        
-  //       }
-  //       
-            
-            
-  //         }
-  //         render() {
-  //           return (
-  //             <Fragment className="App">
-  //       <Nombre inject={this.state.valor} onChange={this.cambiarState}/>
-  //       <Peso />
-  //       <Altura inject={this.state.valor} onChange={this.cambiarState}/>
-  //       <Boton onClick={this.calcBMI} />
-  //       </Fragment>
-  //     );
-  //   }
 }
   
 
